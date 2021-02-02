08f7d0ba0990b611f166bf8b3c7ddc36c08e761b
Sakulthip Rassameecharoentham <sakulthip.r@mail.kmutt.ac.th>
2021-02-02 21:21:35 +0700

    webpage done!

---
+++ 62130500090_groupwork_1/tailwind.config.js
@@ -0,0 +1,22 @@
+//   const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      pink: {
+        100: '#ffafbb',
+        450: '#fd5373',
+        dark: '#EB0029',
+      },
+      gray: {
+        darkest: '#1f2d3d',
+        dark: '#3c4858',
+        DEFAULT: '#6b6b6b',
+        light: '#e0e6ed',
+        lightest: '#f9fafc',
+      }
+    }
+  }
+}
