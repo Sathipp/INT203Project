ae0a765169f27082f0079b668ffb9aa967716810
Sakulthip Rassameecharoentham <sakulthip.r@mail.kmutt.ac.th>
2021-02-03 11:24:29 +0700

    add new work

---
+++ Lab1/profile.js
@@ -0,0 +1,13 @@
+const app = new Vue( {
+     el: "#app",  
+        data: {
+            firstName: "Sakulthip",
+            lastName: "Rassameecharoentham",
+            url: "images/me.jpg",
+            career: "Project Manager",
+            Articles: "33",
+            Followers: "2389",
+            Rating: "8.9"
+        }
+    
+    });
